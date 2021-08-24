ae9801371623de5f00e3ed56d7bd7163dc445d97
PTRL291\Aravind Barri <aravindbarri@gmail.com>
2021-08-24 11:23:49 +0530

    basic requirements are done

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public int health = 0;
+    public Text ScoreText;
+    public Text HealthText;
+    public void CoinScore()
+    {
+        score++;
+        ScoreText.text = "Score: " + score;
+    }
+    public void BottleScore()
+    {
+        health = health + 5;
+        HealthText.text = "Health: " + health;
+    }
+}
